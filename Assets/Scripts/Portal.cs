1b3339552058c25292ac2425e3a01c0e84501988
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-12-13 21:04:41 -0800

    Made portals work in Unity. No back-of-portal culling yet.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Portal : MonoBehaviour {
-	[SerializeField] private Color m_Color;
-	[SerializeField] private MeshRenderer m_BackQuad;
+    [SerializeField] private Color m_Color;
+    [SerializeField] private MeshRenderer m_BackQuad;
+    [SerializeField] private MeshFilter m_StencilMesh;
+
+    public MeshFilter StencilMesh {
+        get { return m_StencilMesh; }
+    }
 
 	void Start() {
 		m_BackQuad.material.color = m_Color;
