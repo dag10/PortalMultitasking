ab4c3a71b49746cb84cd5df67351137746aad4d3
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-12-13 21:04:41 -0800

    Refactored portal cameras to go inside of Portal prefab.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Portal : MonoBehaviour {
+    [SerializeField] private Portal m_LinkedPortal;
     [SerializeField] private Color m_Color;
     [SerializeField] private MeshRenderer m_BackQuad;
     [SerializeField] private MeshFilter m_StencilMesh;
+    [SerializeField] private GameObject m_StencilOverride;
 
     public MeshFilter StencilMesh {
         get { return m_StencilMesh; }
     }
 
-	void Start() {
-		m_BackQuad.material.color = m_Color;
-	}
+    public Portal LinkedPortal {
+        get { return m_LinkedPortal; }
+    }
+
+    public GameObject StencilOverride {
+        get { return m_StencilOverride; }
+    }
+
+    void Start() {
+        m_BackQuad.material.color = m_Color;
+    }
 }
