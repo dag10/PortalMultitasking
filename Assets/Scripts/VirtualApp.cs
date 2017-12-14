caa22827a7a66213fb122a4fa8019253b1aa76cc
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-12-14 13:26:12 -0500

    Some optimization to when portals are enabled.

--- Assets/Scripts/VirtualApp.cs
+++ Assets/Scripts/VirtualApp.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class VirtualApp : MonoBehaviour {
     [SerializeField] private string m_Name;
 
+    [HideInInspector] public bool m_EnableAllPortals = false;
+
     public string Name { get { return m_Name; } }
     public override string ToString() { return m_Name; }
 }
