acebed792f87dfa473a1b053686ccbc91c3b0614
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-12-13 21:04:41 -0800

    Recreated test room, with two non-functioning portals.

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+	[SerializeField] private Color m_Color;
+	[SerializeField] private MeshRenderer m_BackQuad;
+
+	void Start() {
+		m_BackQuad.material.color = m_Color;
+	}
+}
